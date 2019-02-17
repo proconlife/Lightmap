dd4b2697573cc4b32237f2238749b92d9d9bce7d
riko0000 <mariko.s0807@gmail.com>
2019-02-17 16:16:34 +0900

    commit laundmat

---
+++ lib/lightmap_web/controllers/laundromat_controller.ex
@@ -0,0 +1,7 @@
+defmodule LightmapWeb.LaundromatController do
+    use LightmapWeb, :controller
+  
+    def index(conn, _params) do
+      render(conn, "index.html")
+    end
+  end
---
+++ lib/lightmap_web/views/laundromat_view.ex
@@ -0,0 +1,3 @@
+defmodule LightmapWeb.LaundromatView do
+    use LightmapWeb, :view
+  end
